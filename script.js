761ad4bbc0139d0940c253cd062c896213fb99fa
46775 <sujalkawale03@gmail.com>
2024-09-14 16:15:30 +0530

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+var tl = gsap.timeline({
+    repeat:-1
+});
+
+
+tl.to(".imgContainer", {
+    width: "100%",
+    ease: Expo.easeInOut,
+    stagger:2
+}, 'a') 
+.to(".text h1", {
+    top: "0",
+    ease: Expo.easeInOut,
+    stagger:2
+},'a') 
+.to(".text h1", {
+    delay:2,
+    top: "-100px",
+    ease: Expo.easeInOut,
+    stagger:2
+},'a');
+
